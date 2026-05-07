53d30d5072a7d4e40214df6cdd0b7470c50d0223
Ayesha Khan <ayeshaskhan06@gmail.com>
2026-05-07 20:06:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ st.divider()
 age = st.slider('Age',1,80,25)
 sex = st.radio('Gender', ['Male','Female'])
 pclass = st.selectbox('Ticket Class', [1,2,3],
-format_funct = lambda x: f"Class {x} {'(Upper)' if x==1 else '(Middle)' if x==2 else '(Lower)'}")
+format_func = lambda x: f"Class {x} {'(Upper)' if x==1 else '(Middle)' if x==2 else '(Lower)'}")
 fare = st.slider('Fare', 0,500,100)
 
 st.divider()
 
 if st.button("🔮 Predict", use_container_width=True):
     sex_encoded = 1 if sex=='Male' else 0
+    
     input_data = pd.DataFrame([[age,sex_encoded,pclass,fare]],
                               columne=['Age','Sex','Pclass','Fare'])
     prediction = model.predict(input_data)[0]
